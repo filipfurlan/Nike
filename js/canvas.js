1b72ef4ad0f02f7a5b432a3a0e0707005313eb0d
filipfurlan <filip.furlan11@gmail.com>
2025-01-12 17:01:59 +0100

    Add files via upload

--- js/canvas.js
+++ js/canvas.js
@@ -1,18 +1,19 @@
-var c = document.getElementById("canvas");
-var ctx = c.getContext("2d");
-ctx.strokeStyle="rgba(0,0,0,1)";
-ctx.miterLimit=4;
-ctx.scale(0.2,0.2 );
-ctx.translate(-61,-50);
-ctx.fillStyle="rgba(0,0,0,1)";
-ctx.strokeStyle="black";
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
+ctx.scale(0.2, 0.17);
 ctx.beginPath();
-ctx.moveTo(688.26,141.22);
-ctx.bezierCurveTo(240.33,263.33,206.33,285,160.51,263.46);
-ctx.bezierCurveTo(110,239.33,137.33,174.67,154.01,141.46);
-ctx.bezierCurveTo(154.01,141.46,153.43,141,153.43,141);
-ctx.bezierCurveTo(153.43,141,54.67,236.67,62.5,311);
-ctx.bezierCurveTo(71.5,381.5,158.5,373.5,220,350.5);
-ctx.bezierCurveTo(220,350.5,689,141.5,689,141.5);
+ctx.moveTo(183.94,137.5);
+ctx.bezierCurveTo(184,138,152,199,163,230);
+ctx.bezierCurveTo(175,261,209,278,265,262);
+ctx.bezierCurveTo(326,248,720,139,720,139);
+ctx.bezierCurveTo(720,139,332,312,255,344);
+ctx.bezierCurveTo(179,376,137,362,126,355);
+ctx.bezierCurveTo(115,349,75,325,99,259);
+ctx.bezierCurveTo(131,185,184,138,184,138);
+ctx.closePath();
+ctx.fillStyle = 'rgb(222,223,228)';
 ctx.fill();
-ctx.restore();
+ctx.strokeStyle = 'lightgray';
+ctx.stroke();
+
+
